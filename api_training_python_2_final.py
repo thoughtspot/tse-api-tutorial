cf7e066a70334a98125f7ed669b02358f36cf769
Bryant Howell - ThoughtSpot <83678239+bryanthowell-ts@users.noreply.github.com>
2024-03-19 09:39:47 -0500

    Add files via upload
    Updated to remove admin-only commands

--- api_training_python_2_final.py
+++ api_training_python_2_final.py
@@ -3,15 +3,16 @@ import requests
 
 from thoughtspot_rest_api_v1 import *
 
-username = 'username'
-password = 'password'
-server = 'https://{instance}.thoughtspot.cloud'
+username = '{}}'
+password = '{}'
+server = 'https://{}.thoughtspot.cloud'
+org_id = 1613534286
 
 # Create TSRestApiV2 object to do all the requests
 ts: TSRestApiV2 = TSRestApiV2(server_url=server)
 # Try to request token from /auth/token/full and then use token to be the bearer_token of the TSRestApiV2 obj
 try:
-    auth_token_response = ts.auth_token_full(username=username, password=password, validity_time_in_sec=3000)
+    auth_token_response = ts.auth_token_full(username=username, password=password, validity_time_in_sec=3000, org_id=org_id)
     ts.bearer_token = auth_token_response['token']
 except requests.exceptions.HTTPError as e:
     print("Error from the API: ")
@@ -22,12 +23,8 @@ except requests.exceptions.HTTPError as e:
 
 # Create request to /metadata/search to find the Liveboards and Answers matching the name pattern
 # Use the Playground to build your request, then copy/paste in the script
-search_request = {
+search_answers_request = {
     "metadata": [
-    {
-      "name_pattern": "%QA%",
-      "type": "LIVEBOARD"
-    },
     {
       "name_pattern": "%QA%",
       "type": "ANSWER"
@@ -37,81 +34,111 @@ search_request = {
     'record_size': 100000
 }
 
-# Create a List to populate with the IDs of the objects to share to the group
-ids_to_update = []
+search_liveboards_request = {
+ "metadata": [
+    {
+      "name_pattern": "%QA%",
+      "type": "LIVEBOARD"
+    }
+  ],
+    'record_offset': 0,
+    'record_size': 100000
+}
+
+# Create Lists to hold the final set of Answers + Liveboards we want to request SQL from (this isn't required)
+answers_to_find_sql = []
+lbs_to_find_sql =[]
 
 try:
     # Send request to /metadata/search endpoint
-    resp = ts.metadata_search(request=search_request)
+    answers_resp = ts.metadata_search(request=search_answers_request)
     # Iterate through the results from the API response to double-check that the name value matches exactly
-    for item in resp:
+    for item in answers_resp:
         m_name = item["metadata_name"]
         m_id = item["metadata_id"]
         if m_name.find("QA ") != -1:
-            ids_to_update.append(m_id)  # Get final List of IDs to send
-    print("Found the following metadata items to update:")
-    print(ids_to_update)
+            answers_to_find_sql.append(item)  # We'll add the whole object to the new list
+    # print("Found the following Answers to request from:")
+    # print(json.dumps(answers_to_find_sql, indent=2))
 except requests.exceptions.HTTPError as e:
     print("Error from the API: ")
     print(e)
     print(e.response.content)
     exit()
 
-# Check if group exists, create if not
-group_name = "QA"
-# Create the request body for /groups/create endpoint
-groups_request = {
-    "group_identifier": group_name
-}
 
+# Repeat steps above, but for the Liveboards response
 try:
-    # Use /groups/search endpoint to see if group already exists with the expected name
-    resp = ts.groups_search(request=groups_request)
-    # If it does not exist, create the group
-    if len(resp) == 0:
-        # Create the request body for /groups/create endpoint (use Playground, then substitute variables)
-        group_create_req = {
-          "name": group_name,
-          "display_name": group_name,
-          "type": "LOCAL_GROUP",
-          "visibility": "SHARABLE"
-        }
-        # Send the request to /groups/create
-        create_resp = ts.groups_create(request=group_create_req)
-        # Retrieve the GUID for the newly created Group from the response
-        group_id = create_resp['id']
-    else:
-        # There should only be one result in the list
-        group_id = resp[0]['id']
-
-    # Share all content with the QA group
-    # Create the request body for /security/metadata/share in the Playground, then substitute variables
-    share_req = {
-        "metadata_identifiers": ids_to_update,
-        "permissions": [
-            {
-                "principal": {
-                    "identifier": group_id
-                },
-                "share_mode": "READ_ONLY"
-            }
-        ],
-        "emails": [],
-        "message": "",
-        "enable_custom_url": False
-    }
-
-    # Use security/metadata/share endpoint
-    share_resp = ts.security_metadata_share(request=share_req)
-    print(json.dumps(share_resp, indent=2))
-
+    # Send request to /metadata/search endpoint
+    lbs_resp = ts.metadata_search(request=search_liveboards_request)
+    # Iterate through the results from the API response to double-check that the name value matches exactly
+    for item in lbs_resp:
+        m_name = item["metadata_name"]
+        m_id = item["metadata_id"]
+        if m_name.find("QA ") != -1:
+            lbs_to_find_sql.append(item)  # We'll add the whole object to the new list
+    # print("Found the following Liveboards to request from:")
+    # print(json.dumps(lbs_to_find_sql, indent=2))
 except requests.exceptions.HTTPError as e:
     print("Error from the API: ")
     print(e)
-    # Check for a particular 400 error, which previous versions of API did when exact match not found
     print(e.response.content)
-    print("Exiting script due to API error...")
     exit()
 
+#
+# Retrieve and Display all of the SQL queries
+# Here you might make decisions about what to show, format of display
+#
 
+for answer in answers_to_find_sql:
+    try:
+        # metadata_answer_sql doesn't have generic request, it lets you pass in answer_identifier directly
+        sql_resp = ts.metadata_answer_sql(answer_identifier=answer['metadata_id'])
 
+        # Print out the sql_resp structure to inspect for the details you want to retrieve
+        # print(json.dumps(sql_resp, indent=2))
+
+        # Answers only will ever have one sql_query 
+        sql_query = sql_resp['sql_queries'][0]['sql_query']
+        answer_id = sql_resp['metadata_id']
+        answer_name = sql_resp['metadata_name']
+        # Display the information about the Answer and then the query
+        print("Answer:\n{}\n{}\n".format(answer_id, answer_name))
+        print(sql_query)
+        print("---- \n")
+    except requests.exceptions.HTTPError as e:
+        print("Error from the API: ")
+        print(e)
+        print(e.response.content)
+        exit() 
+
+# Retrieve the SQL of each Liveboard
+for lb in lbs_to_find_sql:
+    try:
+        # metadata_liveboard_sql doesn't have generic request, it lets you pass in liveboard_identifier directly
+        sql_resp = ts.metadata_liveboard_sql(liveboard_identifier=lb['metadata_id'])
+
+        lb_id = sql_resp['metadata_id']
+        lb_name = sql_resp['metadata_name']
+
+        print("Liveboard:\n{}\n{}\n".format(lb_id, lb_name))
+
+        print("---- ")
+        # Liveboards have visualizations each with their own query
+        for sql_query in sql_resp['sql_queries']:
+            
+            viz_id = sql_query['metadata_id']
+            viz_name = sql_query['metadata_name']
+            query = sql_query['sql_query']
+
+            print("Viz:\n{}\n{}\n".format(viz_id, viz_name))
+            print(query)
+            print("---- ")
+        print("")
+
+    except requests.exceptions.HTTPError as e:
+        print("Error from the API: ")
+        print(e)
+        print(e.response.content)
+        print(e.request.body)
+        exit() 
