320836b26b90e6adeafa7d555744466b07e39b13
Bryant Howell - ThoughtSpot <83678239+bryanthowell-ts@users.noreply.github.com>
2024-11-14 13:30:56 -0600

    Update api_training_python_2_begin.py

--- api_training_python_2_begin.py
+++ api_training_python_2_begin.py
@@ -3,16 +3,17 @@ import requests
 
 from thoughtspot_rest_api_v1 import *
 
-username = 'username'
-password = 'password'
-server = 'https://{instance}.thoughtspot.cloud'
+username = '{}'
+password = '{}'
+server = 'https://{}.thoughtspot.cloud'
 org_id = 1613534286
 
 # Create TSRestApiV2 object to do all the requests
 ts: TSRestApiV2 = TSRestApiV2(server_url=server)
 # Try to request token from /auth/token/full and then use token to be the bearer_token of the TSRestApiV2 obj
 try:
-    pass
+    auth_token_response = ""
+    ts.bearer_token = ""
 except requests.exceptions.HTTPError as e:
     print("Error from the API: ")
     print(e)
@@ -20,48 +21,82 @@ except requests.exceptions.HTTPError as e:
     print("Exiting script after error...")
     exit()
 
+# 1. Find all Liveboards and Answers with a name that includes '(Sample)'
+
+# Get all of the items with name '(Sample)'
+#  Is this a case-sensitive or insensitive operation? Are we finding anywhere in the name or just at start or end?
+
 # Create request to /metadata/search to find the Liveboards and Answers matching the name pattern
 # Use the Playground to build your request, then copy/paste in the script
-search_answers_request = {
+search_request = {
 }
-
-search_liveboards_request = {
-
-}
-
-# Create Lists to hold the final set of Answers + Liveboards we want to request SQL from (this isn't required)
-answers_to_find_sql = []
-lbs_to_find_sql =[]
 
 try:
     # Send request to /metadata/search endpoint
-    pass
-    # Iterate through the results from the API response to double-check that the name value matches exactly
-    for item in answers_resp:
-        pass
-      # Add the whole object if it matches to the answers_to_find_sql list
-        
-    # print("Found the following Answers to request from:")
-    # print(json.dumps(answers_to_find_sql, indent=2))
+    metadata_resp = ""
 except requests.exceptions.HTTPError as e:
     print("Error from the API: ")
     print(e)
     print(e.response.content)
     exit()
 
+# Create List to hold the final set of Answers + Liveboards we want to tag and share,
+final_list_of_objs =[]
+
+# Iterate through the results from the API response to double-check that the name value matches exactly
+for item in metadata_resp:
+    m_name = ""
+    m_id = ""
+    # Python string find is Case-Sensitive 
+    if m_name.find("(Sample)") != -1:
+       pass # We'll add the whole object to the new List
+
+# 2. Add a tag to each item called 'Tutorial Test'
+
+# Get the ID of the tag called 'Tutorial Test'
+#   What if there is no tag called 'Tutorial Test'?
 
 
-# Repeat steps above, but for the Liveboards response
+#
+# Find the Tag Identifer so we can assign
+# Create new Tag if it doesn't exist
+#
 try:
-    # Send request to /metadata/search endpoint
-    pass
-    # Iterate through the results from the API response to double-check that the name value matches exactly
-    for item in lbs_resp:
-        pass
-        # Add the whole object to the lbs_to_find_sql list
+    tags = ""
+except requests.exceptions.HTTPError as e:
+    print("Error from the API: ")
+    print(e)
+    print(e.response.content)
+    exit()
 
-    # print("Found the following Liveboards to request from:")
-    # print(json.dumps(lbs_to_find_sql, indent=2))
+if len(tags) == 0:
+    try:
+        new_tag = ""
+        tag_id = new_tag['id']
+    except requests.exceptions.HTTPError as e:
+        print("Error from the API: ")
+        print(e)
+        print(e.response.content)
+        exit()
+else:
+    tag_id = tags[0]['id']
+
+# Assign the tag to the items
+
+try:
+   # When we copied from the Playground, we realize the format of the `metadata` section is an Array of Objects,
+   # which needs to be a List of Dicts in Python syntax [ {"identifier": metadata_id}, ...]
+   
+   tag_metadata_section = []
+   # Iterate through each object and make the Dict in create format
+   for obj in final_list_of_objs:
+        pass
+
+   assign_req = {
+
+   }
+
+   assign_resp = ts.tags_assign(requst=assign_req)
 except requests.exceptions.HTTPError as e:
     print("Error from the API: ")
     print(e)
@@ -69,61 +104,22 @@ except requests.exceptions.HTTPError as e:
     exit()
 
 #
-# Retrieve and Display all of the SQL queries
-# Here you might make decisions about what to show, format of display
+# Extra credit assigment below:
 #
 
-for answer in answers_to_find_sql:
-    try:
-        # metadata_answer_sql doesn't have generic request, it lets you pass in answer_identifier directly
+# 3. Give read-only access to a 'Tutorial Test' group
+
+# 3.1 Get the ID of the group called 'Tutorial Test'
+#  Is 'Tutorial Test' the name or the display_name property?
+#  What if there is no group called 'Tutorial Test'?
 
 
-        # Print out the sql_resp structure to inspect for the details you want to retrieve
-        # print(json.dumps(sql_resp, indent=2))
+#
+# Find the Group Identifer so we can share
+# Create new Group if it doesn't exist
+#
 
-        # Answers only will ever have one sql_query 
-        sql_query = 
-        answer_id = 
-        answer_name = 
-        # Display the information about the Answer and then the query
-        print("".format(answer_id, answer_name))
-        print(sql_query)
-        print("---- \n")
-    except requests.exceptions.HTTPError as e:
-        print("Error from the API: ")
-        print(e)
-        print(e.response.content)
-        exit() 
 
-# Retrieve the SQL of each Liveboard
-for lb in lbs_to_find_sql:
-    try:
-        # metadata_liveboard_sql doesn't have generic request, it lets you pass in liveboard_identifier directly
-        sql_resp = ts.metadata_liveboard_sql(liveboard_identifier=lb['metadata_id'])
+# 3.2 Share content as READ-ONLY to the group
 
-        lb_id = 
-        lb_name = 
-
-        # Display the information about the Liveboard before iterating through the visualizations for their details
-        print("".format(lb_id, lb_name))
-        print("---- ")
-
-        # Liveboards have visualizations each with their own query, iterate through each query to find the details
-        for sql_query in sql_resp['sql_queries']:
-            
-            viz_id = 
-            viz_name = 
-            query = 
-
-            # Display the information about the Viz and then the query
-            print("".format(viz_id, viz_name))
-            print(query)
-            print("---- ")
-        print("")
-
-    except requests.exceptions.HTTPError as e:
-        print("Error from the API: ")
-        print(e)
-        print(e.response.content)
-        print(e.request.body)
-        exit() 
+#
